881be0b44f04ea0e17c62b7bb330d284cbe431b1
Jadon H <hawcroftj@gmail.com>
2018-06-05 16:28:22 -0500

    Modified index.php to be more responsive on small screens.

--- index.php
+++ index.php
@@ -17,25 +17,25 @@
     
     <body id="body" class="colour-p">
         <?php include('nav.php'); ?>
-        <div id="banner" class="jumotron text-center border-top border-bottom border-dark pb-3 colour-l1 text-d2">
+        <div id="banner" class="jumotron jumbotron-fluid text-center border-top border-bottom border-dark pb-3 colour-l1 text-d2">
             <h1 class="display-1">hawcroft.ca</h1>
-            <h1><small>A perpetual work in progress. Not optimized for mobile.</small></h1>
+            <h1><small>My personal JavaScript sandbox. Thanks for stopping by!</small></h1>
         </div>
         <div class="container rounded mt-5 p-5 ">
             <div class="row">
-                <div class="col text-center text-bulge">
+                <div class="col-sm-4 text-center text-bulge">
                     <a href="https://www.linkedin.com/in/jadon-hawcroft-a66013115/">
                         <img src="res/LinkedIn.png"/>
                     </a>
                     <p class="text-l2">LinkedIn</p>
                 </div>
-                <div class="col text-center text-bulge">                    
+                <div class="col-sm-4 text-center text-bulge">                    
                     <a href="https://github.com/hawcroftj">
                         <img src="res/GitHub.png"/>
                     </a>
                     <p class="mt-2 text-l2">GitHub</p>
                 </div>
-                <div class="col text-center text-bulge">
+                <div class="col-sm-4 text-center text-bulge">
                     <a href="#">
                         <img src="res/jh_120.png"/>
                     </a>
