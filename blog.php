a6654a62c77148add252d9fad16597cbec281320
Jadon H <hawcroftj@gmail.com>
2018-06-28 17:37:24 -0500

    Blog page is now populated with hardcoded test data. Each post is given a row in the container.

--- blog.php
+++ blog.php
@@ -1,5 +1,16 @@
 <?php
 
+$test_data = [
+    "blog1" => [
+        "title" => "Blog Post 1",
+        "content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus maximus tincidunt. Ut scelerisque imperdiet dui vitae feugiat. Donec ullamcorper, tellus vel rhoncus tincidunt, sapien velit lacinia purus, et luctus sem lacus eget ex. Donec non consectetur quam. Quisque at elit et tortor dapibus rhoncus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus at urna id quam tincidunt dictum vel at erat."    
+    ],
+    "blog2" => [
+        "title" => "Blog Post 2",
+        "content" => "Integer tellus velit, facilisis eu nunc at, aliquet eleifend nibh. Donec tempor lacus at sem rutrum semper. Vestibulum libero risus, hendrerit quis libero in, auctor vestibulum eros. Etiam porta accumsan quam, et pulvinar est pulvinar a. Maecenas et eros in ante pharetra consequat vel eget nibh. Phasellus fermentum volutpat metus ut elementum. Sed ligula mi, laoreet sit amet maximus tincidunt, pretium ac purus."
+    ]
+]
+
 ?>
 
 <!DOCTYPE html>
@@ -18,6 +29,21 @@
     <body class="colour-p">
         <?php include('nav.php'); ?>
         
+        <!-- Check for blog posts -->
+        <?php if(!empty($test_data)): ?>
+            <div class="container">
+            <!-- Add post(s) to the container -->
+            <?php foreach($test_data as $blog): ?>
+                <div class="row mt-2 colour-l1">
+                    <div class="col-sm-10 offset-1 p-2 colour-l1">
+                        <p><?= $blog["title"] ?></p>
+                        <p><?= $blog["content"] ?></p>
+                    </div>
+                </div>
+            <?php endforeach ?>
+            </div>
+        <?php endif ?>
+
         <?php include('footer.php'); ?>
         <script src="js/jquery.min.js"></script>
         <script src="js/bootstrap.min.js"></script>
