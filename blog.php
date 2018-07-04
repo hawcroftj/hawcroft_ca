b20794828ac364a0773dbe60929a56e2cef955e2
Jadon H <hawcroftj@gmail.com>
2018-07-03 19:32:05 -0500

    Modified blog page to display 'Under Construction' message.

--- blog.php
+++ blog.php
@@ -1,16 +1,16 @@
 <?php
 
-require('connect.php');
+//require('connect.php');
 
 $test_data = [
-    "blog1" => [
-        "title" => "Blog Post 1",
-        "content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus maximus tincidunt. Ut scelerisque imperdiet dui vitae feugiat. Donec ullamcorper, tellus vel rhoncus tincidunt, sapien velit lacinia purus, et luctus sem lacus eget ex. Donec non consectetur quam. Quisque at elit et tortor dapibus rhoncus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus at urna id quam tincidunt dictum vel at erat."    
-    ],
-    "blog2" => [
-        "title" => "Blog Post 2",
-        "content" => "Integer tellus velit, facilisis eu nunc at, aliquet eleifend nibh. Donec tempor lacus at sem rutrum semper. Vestibulum libero risus, hendrerit quis libero in, auctor vestibulum eros. Etiam porta accumsan quam, et pulvinar est pulvinar a. Maecenas et eros in ante pharetra consequat vel eget nibh. Phasellus fermentum volutpat metus ut elementum. Sed ligula mi, laoreet sit amet maximus tincidunt, pretium ac purus."
-    ]
+    // "blog1" => [
+    //     "title" => "Under Development",
+    //     "content" => "I'm working on getting a database set up for the blogs page. Everything you see here is a work in progress."
+    // ],
+    // "blog2" => [
+    //     "title" => "Test Blog Post",
+    //     "content" => "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In luctus maximus tincidunt. Ut scelerisque imperdiet dui vitae feugiat. Donec ullamcorper, tellus vel rhoncus tincidunt, sapien velit lacinia purus, et luctus sem lacus eget ex. Donec non consectetur quam. Quisque at elit et tortor dapibus rhoncus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus at urna id quam tincidunt dictum vel at erat. Integer tellus velit, facilisis eu nunc at, aliquet eleifend nibh. Donec tempor lacus at sem rutrum semper. Vestibulum libero risus, hendrerit quis libero in, auctor vestibulum eros. Etiam porta accumsan quam, et pulvinar est pulvinar a. Maecenas et eros in ante pharetra consequat vel eget nibh. Phasellus fermentum volutpat metus ut elementum. Sed ligula mi, laoreet sit amet maximus tincidunt, pretium ac purus."
+    // ]
 ]
 
 ?>
@@ -25,12 +25,20 @@ $test_data = [
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/custom.css">
-        <title>Blog | Hawcroft</title>
+        <title>Work in Progress | Hawcroft</title>
     </head>
 
     <body id="body" class="colour-p">
         <?php include('nav.php'); ?>
         
+        <!-- Temporary 'Work in Progress' message -->
+        <div id="banner" class="jumotron jumbotron-fluid text-center border-top border-bottom border-dark pt-1 pb-1 colour-l1 text-d2">
+            <h1 class="display-1 font-weight-light">Under Construction</h1>
+            <p class="lead">This page will hopefully display blog posts someday (if I can successfully configure a database).
+                Feel free to check back here for updates - everything is right on schedule.*</p>
+            <p>*there is currently no schedule.</p>
+        </div>
+
         <!-- Check for blog posts -->
         <?php if(!empty($test_data)): ?>
             <div class="container">
